549add4efd35edfc149e202602b62bf913b9e88c
Alexander Ames <alexanderthomasames@gmail.com>
2021-12-13 13:58:33 -0700

    * create user and item types

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: true
+    field :email, String, null: true
+    field :items, [Types::ItemType], null: true
+  end
+end
